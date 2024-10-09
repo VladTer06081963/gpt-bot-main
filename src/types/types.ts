3fac830a50062c0fe20948567932ad9ad8debc44
Wlad <patrol200611@ukr.net>
2024-10-09 16:27:31 +0300

    Garbage collection

--- src/types/types.ts
+++ src/types/types.ts
@@ -33,9 +33,12 @@ export enum AiModels {
 }
 
 export enum AiModelsLabels {
-  GPT_3_5_TURBO = 'GPT-3.5 Turbo',
-  GPT_4O = 'GPT-4o',
-  GPT_4O_MINI = 'GPT-4o-mini',
+  GPT_3_5_TURBO = 'gpt-3.5-turbo-0125',
+  GPT_4O = 'gpt-4o',
+  GPT_4O_MINI = 'gpt-4o-mini',
+  // GPT_3_5_TURBO = 'GPT-3.5 Turbo',
+  // GPT_4O = 'GPT-4o',
+  // GPT_4O_MINI = 'GPT-4o-mini',
   // O1_PREVIEW = 'o1-preview',
   // O1_MINI = 'o1-mini',
 }
