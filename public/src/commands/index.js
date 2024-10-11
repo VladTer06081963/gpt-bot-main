d98d1252642f77be443a58fd66d74a84c6f68c22
Wlad <patrol200611@ukr.net>
2024-10-11 16:04:32 +0300

    tsc

--- public/src/commands/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAnalytics = exports.changeModel = void 0;
-var changeModel_1 = require("./changeModel");
-Object.defineProperty(exports, "changeModel", { enumerable: true, get: function () { return changeModel_1.changeModel; } });
-var analytics_1 = require("./analytics");
-Object.defineProperty(exports, "getAnalytics", { enumerable: true, get: function () { return analytics_1.getAnalytics; } });
--- public/src/types/typeguards.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isValidAiModel = void 0;
-const types_1 = require("./types");
-const isValidAiModel = (model) => {
-    return typeof model === 'string' && model in types_1.AiModels;
-};
-exports.isValidAiModel = isValidAiModel;
